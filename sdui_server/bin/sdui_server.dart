1afc750e28892823b2783b398731b1510e81a99f
Garvit1107 <garvitbudhiraja1@gmail.com>
2025-12-24 16:42:31 +0530

    Initialising project

---
+++ sdui_server/bin/sdui_server.dart
@@ -0,0 +1,5 @@
+import 'package:sdui_server/sdui_server.dart' as sdui_server;
+
+void main(List<String> arguments) {
+  print('Hello world: ${sdui_server.calculate()}!');
+}
---
+++ sdui_server/test/sdui_server_test.dart
@@ -0,0 +1,8 @@
+import 'package:sdui_server/sdui_server.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
